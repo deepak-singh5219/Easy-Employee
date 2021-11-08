0a226615a7d8a5922983965e5ee24f099c5349de
Deepak Singh <singhdeepak5219@gmail.com>
2021-11-09 05:09:46 +0530

    Added Features : Leader Route, Update Self

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const dbConnection = require('./configs/db-config');
 const authRoute = require('./routes/auth-route');
 const adminRoute = require('./routes/admin-route');
 const employeeRoute = require('./routes/employee-route');
+const leaderRoute = require('./routes/leader-route');
 const errorMiddleware = require('./middlewares/error-middleware');
 const ErrorHandler = require('./utils/error-handler');
 const {auth, authRole} = require('./middlewares/auth-middleware');
@@ -32,6 +33,7 @@ app.use(cookieParser());
 app.use('/api/auth',authRoute);
 app.use('/api/admin',auth,authRole('admin'),adminRoute);
 app.use('/api/employee',auth,authRole('employee'),employeeRoute);
+app.use('/api/leader',auth,authRole('leader'),leaderRoute);
 
 
 app.use('/storage',express.static('storage'))
