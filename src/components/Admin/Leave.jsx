a4a404af498dc2521ffcc2a708901812b46e178f
Deepak Singh <singhdeepak5219@gmail.com>
2023-11-20 11:13:07 +0530

    improvement

--- src/components/Admin/Leave.jsx
+++ src/components/Admin/Leave.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { updateLeave, viewLeaves } from '../../http';
 import { toast } from 'react-toastify';
+import Loading from '../Loading';
+
 
 
 
@@ -56,68 +58,75 @@ const Leave = () => {
     }
     
   return (
-    <div className="main-content">
-    <section className="section">
-            <div className="card">
-              <div className="card-header d-flex justify-content-between">
-                <h4>Application on {application?.appliedDate}</h4>
+    <>
+    {
+      application? (<div className="main-content">
+      <section className="section">
+              <div className="card">
+                <div className="card-header d-flex justify-content-between">
+                  <h4>Application on {application?.appliedDate}</h4>
+                </div>
               </div>
-            </div>
-
-            <div className="col-md-9">
-                    <table className='table'>
-                      <tbody>
-                        <tr>
-                          <th>Title</th>
-                          <td>{application?.title}</td>
-                        </tr>
-                        <tr>
-                          <th>Type</th>
-                          <td>{application?.type}</td>
-                        </tr>
-                        <tr>
-                          <th>Reason</th>
-                          <td>{application?.reason}</td>
-                        </tr>
-                        <tr>
-                          <th>Start Date</th>
-                          <td>
-                           {application?.startDate}
-                        </td>
-                        </tr>
-                        <tr>
-                          <th>End Date</th>
-                          <td>
-                           {application?.endDate}
-                        </td>
-                        </tr>
-                        <tr>
-                          <th>Applied Date</th>
-                          <td>
-                           {application?.appliedDate}
-                        </td>
-                        </tr>
-                        <tr>
-                          <th>Period</th>
-                          <td>
-                           {application?.period}
-                        </td>
-                        </tr>
-                        <tr>
-                          <th>Status</th>
-                          <td className={`${application?.adminResponse==="Rejected"?"text-danger":application?.adminResponse==="Pending"?"text-primary":"text-success"}`}>
-                           {application?.adminResponse}
-                        </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    <button onClick={approveApplication} className={`${application?.adminResponse==="Approved"?"btn-secondary":"btn-success"} btn btn-lg m-4 btn-icon-split`}>Approve</button>
-                    <button onClick={rejectApplication} className={`${application?.adminResponse==="Rejected"?"btn-secondary":"btn-danger"} btn btn-lg m-4 btn-icon-split`}>Reject</button>
-
-                  </div>
-    </section>
-  </div>
-  )
+  
+              <div className="col-md-9">
+                      <table className='table'>
+                        <tbody>
+                          <tr>
+                            <th>Title</th>
+                            <td>{application?.title}</td>
+                          </tr>
+                          <tr>
+                            <th>Type</th>
+                            <td>{application?.type}</td>
+                          </tr>
+                          <tr>
+                            <th>Reason</th>
+                            <td>{application?.reason}</td>
+                          </tr>
+                          <tr>
+                            <th>Start Date</th>
+                            <td>
+                             {application?.startDate}
+                          </td>
+                          </tr>
+                          <tr>
+                            <th>End Date</th>
+                            <td>
+                             {application?.endDate}
+                          </td>
+                          </tr>
+                          <tr>
+                            <th>Applied Date</th>
+                            <td>
+                             {application?.appliedDate}
+                          </td>
+                          </tr>
+                          <tr>
+                            <th>Period</th>
+                            <td>
+                             {application?.period}
+                          </td>
+                          </tr>
+                          <tr>
+                            <th>Status</th>
+                            <td className={`${application?.adminResponse==="Rejected"?"text-danger":application?.adminResponse==="Pending"?"text-primary":"text-success"}`}>
+                             {application?.adminResponse}
+                          </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      <button onClick={approveApplication} className={`${application?.adminResponse==="Approved"?"btn-secondary":"btn-success"} btn btn-lg m-4 btn-icon-split`}>Approve</button>
+                      <button onClick={rejectApplication} className={`${application?.adminResponse==="Rejected"?"btn-secondary":"btn-danger"} btn btn-lg m-4 btn-icon-split`}>Reject</button>
+  
+                    </div>
+      </section>
+    </div>)
+    : <Loading/>
+  
+    }
+    </>
+    
+      )
 }
 
 export default Leave;
